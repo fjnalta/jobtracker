a6681f77a3e139ef17bc82b2f0bd1909e07d83b4
Max Blatt <max.blatt@web.de>
2016-04-07 13:00:51 +0200

    Leeres Gwt Projekt erstellen.

---
+++ net.greenbeansit.timer/src/net/greenbeansit/timer/client/Net_greenbeansit_timer.java
@@ -0,0 +1,28 @@
+package net.greenbeansit.timer.client;
+
+import org.gwtbootstrap3.client.ui.Button;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class Net_greenbeansit_timer implements EntryPoint {
+	/**
+	 * The message displayed to the user when the server cannot be reached or
+	 * returns an error.
+	 */
+	private static final String SERVER_ERROR = "An error occurred while "
+			+ "attempting to contact the server. Please check your network " + "connection and try again.";
+
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+		
+		Button b = new Button("Test");
+		
+		RootLayoutPanel.get().add(b);
+	}
+}
