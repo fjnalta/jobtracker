66dc3eca4cbbe73db851f39686cf1fa3f3bcf6bf
Max Blatt <max.blatt@web.de>
2016-04-19 11:18:13 +0200

    Set up project again
    Move the files from the old net.green.beans.timer into the new
    JobTracker project.
    No change of the functionality.

---
+++ JobTracker/src/net/greenbeansit/jobtracker/client/Application.java
@@ -0,0 +1,109 @@
+package net.greenbeansit.jobtracker.client;
+
+import org.gwtbootstrap3.client.ui.AnchorListItem;
+import org.gwtbootstrap3.client.ui.Container;
+import org.gwtbootstrap3.client.ui.NavbarNav;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiTemplate;
+import com.google.gwt.user.client.History;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+
+import net.greenbeansit.jobtracker.client.components.HomePage;
+import net.greenbeansit.jobtracker.client.components.ProjectPage;
+
+/**
+ * Represents the main widget of the application.
+ */
+class Application extends Composite
+{
+	private static ApplicationUiBinder uiBinder = 
+			GWT.create(ApplicationUiBinder.class);
+	
+	@UiTemplate("Application.ui.xml") 
+	interface ApplicationUiBinder extends UiBinder<Widget, Application>
+	{
+		
+	}
+	
+	
+	@UiField
+	Container mainContainer;
+	
+	@UiField
+	AnchorListItem navHome;
+	
+	@UiField
+	AnchorListItem navProject;
+	
+	@UiField
+	NavbarNav nav;
+	
+	
+	public Application()
+	{
+		initWidget(uiBinder.createAndBindUi(this));
+		
+		History.addValueChangeHandler(new HistoryValueChangeHandler());
+		
+		History.fireCurrentHistoryState();
+	}
+	
+	
+	private class HistoryValueChangeHandler implements ValueChangeHandler<String>
+	{
+
+		@Override
+		public void onValueChange(ValueChangeEvent<String> event)
+		{
+			deactivateAllNavButtons();
+			
+			switch(event.getValue())
+			{
+				case "project":
+					navProject.setActive(true);
+					
+					removeActiveContentContainer();
+					
+					addWidgetAsContentPage(new ProjectPage());
+					break;
+					
+				default: //home
+					navHome.setActive(true);
+					
+					removeActiveContentContainer();
+					
+					addWidgetAsContentPage(new HomePage());
+					break;
+			}
+			
+		}
+		
+		private void deactivateAllNavButtons()
+		{
+			for(int i= 0; i < nav.getWidgetCount(); i++)
+			{
+				Widget widget = nav.getWidget(i);
+				if(widget instanceof AnchorListItem)
+					((AnchorListItem)widget).setActive(false);
+			}
+		}
+		
+		private void removeActiveContentContainer()
+		{
+			for(int i = 1; i < mainContainer.getWidgetCount(); i++)
+				mainContainer.remove(i);
+			
+		}
+		
+		private void addWidgetAsContentPage(Widget widget)
+		{
+			mainContainer.add(widget);
+		}
+	}
+}
---
+++ JobTracker/src/net/greenbeansit/jobtracker/client/components/ProjectPage.java
@@ -0,0 +1,29 @@
+package net.greenbeansit.jobtracker.client.components;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HasText;
+import com.google.gwt.user.client.ui.Widget;
+
+public class ProjectPage extends Composite
+{
+	private static ProjectPageUiBinder uiBinder = GWT.create(ProjectPageUiBinder.class);
+
+	interface ProjectPageUiBinder extends UiBinder<Widget, ProjectPage>
+	{
+		
+	}
+
+	
+	public ProjectPage()
+	{
+		initWidget(uiBinder.createAndBindUi(this));
+	}
+
+}
