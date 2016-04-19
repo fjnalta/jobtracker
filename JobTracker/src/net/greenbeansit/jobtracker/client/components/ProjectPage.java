132ee193e9ced50e391a5abcb3faad708db87072
Max Blatt <max.blatt@web.de>
2016-04-20 00:52:11 +0200

    Add ProjectBudgetBar

--- JobTracker/src/net/greenbeansit/jobtracker/client/components/ProjectPage.java
+++ JobTracker/src/net/greenbeansit/jobtracker/client/components/ProjectPage.java
@@ -1,5 +1,7 @@
 package net.greenbeansit.jobtracker.client.components;
 
+import org.gwtbootstrap3.client.ui.Row;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
@@ -11,6 +13,8 @@ import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HasText;
 import com.google.gwt.user.client.ui.Widget;
 
+import net.greenbeansit.jobtracker.client.components.widgets.ProjectBudgetBar;
+
 public class ProjectPage extends Composite
 {
 	private static ProjectPageUiBinder uiBinder = GWT.create(ProjectPageUiBinder.class);
@@ -21,9 +25,17 @@ public class ProjectPage extends Composite
 	}
 
 	
+	@UiField
+	Row containerBudgetDiagrams;
+	
+	
 	public ProjectPage()
 	{
 		initWidget(uiBinder.createAndBindUi(this));
+		
+		containerBudgetDiagrams.add(new ProjectBudgetBar(500, 1000, "Projekt X"));
+		containerBudgetDiagrams.add(new ProjectBudgetBar(920, 1000, "Projekt Y"));
+		containerBudgetDiagrams.add(new ProjectBudgetBar(800, 1000, "Projekt Z"));
 	}
 
 }
