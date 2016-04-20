bf5a34dd99e2c9185567b0909bbab3364ae9a3a5
Max Blatt <max.blatt@web.de>
2016-04-20 16:55:23 +0200

    Add Select widgets in ProjectPage
    Elements are only shown after clicking "Home" and going bag.
    Need to be fixed.

--- JobTracker/src/net/greenbeansit/jobtracker/client/components/ProjectPage.java
+++ JobTracker/src/net/greenbeansit/jobtracker/client/components/ProjectPage.java
@@ -1,16 +1,12 @@
 package net.greenbeansit.jobtracker.client.components;
 
+import org.gwtbootstrap3.client.ui.Button;
 import org.gwtbootstrap3.client.ui.Row;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.uibinder.client.UiHandler;
-import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.HasText;
 import com.google.gwt.user.client.ui.Widget;
 
 import net.greenbeansit.jobtracker.client.components.widgets.ProjectBudgetBar;
@@ -36,6 +32,8 @@ public class ProjectPage extends Composite
 		containerBudgetDiagrams.add(new ProjectBudgetBar(500, 1000, "Projekt X"));
 		containerBudgetDiagrams.add(new ProjectBudgetBar(920, 1000, "Projekt Y"));
 		containerBudgetDiagrams.add(new ProjectBudgetBar(800, 1000, "Projekt Z"));
+		
+		
 	}
 
 }
