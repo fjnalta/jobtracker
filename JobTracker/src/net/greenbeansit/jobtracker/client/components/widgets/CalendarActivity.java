a79b844917347aa9392eca0d82955ed3945a59a4
Max Blatt <max.blatt@web.de>
2016-04-21 11:16:24 +0200

    Fix render issues

--- JobTracker/src/net/greenbeansit/jobtracker/client/components/widgets/CalendarActivity.java
+++ JobTracker/src/net/greenbeansit/jobtracker/client/components/widgets/CalendarActivity.java
@@ -1,7 +1,10 @@
 package net.greenbeansit.jobtracker.client.components.widgets;
 
+import java.util.Date;
+
 import org.gwtbootstrap3.client.ui.html.ClearFix;
 import org.gwtbootstrap3.extras.fullcalendar.client.ui.CalendarConfig;
+import org.gwtbootstrap3.extras.fullcalendar.client.ui.Event;
 import org.gwtbootstrap3.extras.fullcalendar.client.ui.FullCalendar;
 import org.gwtbootstrap3.extras.fullcalendar.client.ui.GeneralDisplay;
 import org.gwtbootstrap3.extras.fullcalendar.client.ui.Language;
@@ -10,43 +13,63 @@ import org.gwtbootstrap3.extras.fullcalendar.client.ui.ViewOption;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 
-public class CalendarActivity extends Composite
-{
+public class CalendarActivity extends Composite {
 
 	private static CalendarWidgetUiBinder uiBinder = GWT.create(CalendarWidgetUiBinder.class);
 
-	interface CalendarWidgetUiBinder extends UiBinder<Widget, CalendarActivity>
-	{
-		
+	interface CalendarWidgetUiBinder extends UiBinder<Widget, CalendarActivity> {
+
 	}
 
 	@UiField
 	ClearFix container;
-	
+
 	FullCalendar calendar;
-	
-	public CalendarActivity() 
-	{
+
+	public CalendarActivity() {
 		initWidget(uiBinder.createAndBindUi(this));
-		
-		
-		//calendar.addStyleName("{style.dashBoardCalendar}");
-		//calendar.setSize("100%", "400px");
-		CalendarConfig config = new CalendarConfig();
-		config.setLangauge(Language.German);
-		
-		GeneralDisplay generalDisplay = new GeneralDisplay();
-		generalDisplay.setHeight(600);
-		
-		config.setGeneralDisplay(generalDisplay);
-		
-		calendar = new FullCalendar("fullCalendar", ViewOption.agendaWeek, config, false);
-		
-		container.add(calendar);
+
+		new RenderTimer().schedule(0);
 	}
 
+	@Override
+	protected void onLoad() {
+		super.onLoad();
+		
+		
+	}
+	
+	private class RenderTimer extends Timer
+	{
 
+		@Override
+		public void run()
+		{
+			CalendarConfig config = new CalendarConfig();
+			config.setLangauge(Language.German);
+
+			GeneralDisplay generalDisplay = new GeneralDisplay();
+			generalDisplay.setHeight(600);
+
+			config.setGeneralDisplay(generalDisplay);
+			
+			calendar = new FullCalendar("fullCalendar", ViewOption.agendaWeek, config, false);
+
+			Event event = new Event("event", "Event");
+
+			event.setStart(new Date());
+			event.setEnd(new Date());
+
+			container.add(calendar);
+
+			calendar.addEvent(event);
+			calendar.render(); 
+			
+		}
+		
+	}
 }
