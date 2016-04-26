9d809f3623f4537b282db44194c959c8d9b92059
Toures <mike.hukiewitz@gmx.de>
2016-04-26 12:21:21 +0200

    Move merged files from frontend_work to frontend

--- JobTracker/src/net/greenbeansit/jobtracker/client/components/widgets/UtilizationWidget.java
+++ JobTracker/src/net/greenbeansit/jobtracker/client/components/widgets/UtilizationWidget.java
@@ -1,17 +1,13 @@
 package net.greenbeansit.jobtracker.client.components.widgets;
 
-<<<<<<< HEAD
-=======
+import com.google.gwt.canvas.dom.client.Context2d;
+
 import org.gwtbootstrap3.client.ui.html.ClearFix;
 
-import com.google.gwt.canvas.dom.client.Context2d;
->>>>>>> refs/remotes/origin/frontend_prototype
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
-<<<<<<< HEAD
-=======
+
 import com.google.gwt.uibinder.client.UiField;
->>>>>>> refs/remotes/origin/frontend_prototype
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 import com.googlecode.gwt.charts.client.gauge.Gauge;
--- JobTracker/src/net/greenbeansit/jobtracker/client/components/widgets/WorkDiscriptionWidget.java
+++ JobTracker/src/net/greenbeansit/jobtracker/client/components/widgets/WorkDiscriptionWidget.java
@@ -3,17 +3,7 @@ package net.greenbeansit.jobtracker.client.components.widgets;
 import org.gwtbootstrap3.extras.select.client.ui.Select;
 
 import com.google.gwt.core.client.GWT;
-<<<<<<< HEAD
-=======
-import com.google.gwt.core.client.Scheduler;
-import com.google.gwt.core.client.Scheduler.ScheduledCommand;
->>>>>>> refs/remotes/origin/frontend_prototype
 import com.google.gwt.uibinder.client.UiBinder;
-<<<<<<< HEAD
-=======
-import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.Timer;
->>>>>>> refs/remotes/origin/frontend_prototype
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 
