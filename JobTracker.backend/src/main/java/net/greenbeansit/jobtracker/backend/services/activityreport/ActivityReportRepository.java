c48ab2a8cff35e38c7f8b5b149efc0e4fa83b402
Max Blatt <max.blatt@web.de>
2016-05-02 15:01:47 +0200

    Revert "fix previous commit"
    This reverts commit 74cf59df0600fd85270fbd7f7965042d035383d1.

--- JobTracker.backend/src/main/java/net/greenbeansit/jobtracker/backend/services/activityreport/ActivityReportRepository.java
+++
@@ -1,9 +0,0 @@
-package net.greenbeansit.jobtracker.backend.services.activityreport;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ActivityReportRepository extends CrudRepository<ActivityReportEntity, Long> {
-
-	//TODO: Vervollständigen, checken wohin das gehört
-	ActivityReportEntity findByLastname(String lastname);
-}
