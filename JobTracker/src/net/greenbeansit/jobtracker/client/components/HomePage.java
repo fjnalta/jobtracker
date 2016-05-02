706faacbe82065ae4268a2da73db4f467d0c7730
Max Blatt <max.blatt@web.de>
2016-05-02 16:43:04 +0200

    Fix errors in class HomePage

--- JobTracker/src/net/greenbeansit/jobtracker/client/components/HomePage.java
+++ JobTracker/src/net/greenbeansit/jobtracker/client/components/HomePage.java
@@ -6,33 +6,32 @@ import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 
-import net.greenbeansit.jobtracker.client.components.widgets.CalendarWidget;
 import net.greenbeansit.jobtracker.client.components.widgets.JobsWidget;
 import net.greenbeansit.jobtracker.client.components.widgets.UtilizationWidget;
 import net.greenbeansit.jobtracker.client.components.widgets.WorkDiscriptionWidget;
 import net.greenbeansit.jobtracker.shared.Job;
 
-public class HomePage extends Composite{
+public class HomePage extends Composite
+{
 
-	private static HomePageUiBinder uiBinder = GWT.create(HomePageUiBinder.class);
+	private static HomePageUiBinder uiBinder = GWT
+			.create(HomePageUiBinder.class);
 
-	interface HomePageUiBinder extends UiBinder<Widget, HomePage> {
+	interface HomePageUiBinder extends UiBinder<Widget, HomePage>
+	{
 	}
 
+	@UiField
+	JobsWidget				jobsWidget;
 
 	@UiField
-	JobsWidget jobsWidget;
+	UtilizationWidget		utilizationWidget;
 
 	@UiField
-	CalendarWidget calendarWidget;
+	WorkDiscriptionWidget	workDiscriptionWidget;
 
-	@UiField
-	UtilizationWidget utilizationWidget;
-
-	@UiField
-	WorkDiscriptionWidget workDiscriptionWidget;
-
-	public HomePage() {
+	public HomePage()
+	{
 		initWidget(uiBinder.createAndBindUi(this));
 
 		initialize();
@@ -43,9 +42,9 @@ public class HomePage extends Composite{
 	 * function called in constuctor on initializing the widgets and connect
 	 * them with the homePage
 	 */
-	private void initialize() {
+	private void initialize()
+	{
 		jobsWidget.registerObserver(this);
-		calendarWidget.registerObserver(this);
 		utilizationWidget.registerObserver(this);
 		workDiscriptionWidget.registerObserver(this);
 	}
@@ -55,7 +54,8 @@ public class HomePage extends Composite{
 	 * calendarWidget,JobsWidget and WorkDiscriptionWidget then it sends it to
 	 * the backend
 	 */
-	public void bookActivityReport() {
+	public void bookActivityReport()
+	{
 		Job jobToSave = jobsWidget.getSelectedJob();
 		// ActivityReport newReport = calendarWidget.getNewActivityReport();
 
@@ -64,7 +64,8 @@ public class HomePage extends Composite{
 	/**
 	 * update Widgets based on selected Job
 	 */
-	public void update(Job selectedJob) {
+	public void update(Job selectedJob)
+	{
 
 	}
 
