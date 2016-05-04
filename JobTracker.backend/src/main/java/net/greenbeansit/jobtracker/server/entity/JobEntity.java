55fd874c672f4fc8921c51d5bcee3f96cb154493
Max Blatt <max.blatt@web.de>
2016-05-04 21:22:40 +0200

    Remove unused imports in JobEntity.java

--- JobTracker.backend/src/main/java/net/greenbeansit/jobtracker/server/entity/JobEntity.java
+++ JobTracker.backend/src/main/java/net/greenbeansit/jobtracker/server/entity/JobEntity.java
@@ -6,8 +6,6 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
