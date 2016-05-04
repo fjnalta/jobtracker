8e2ab1f444265b8120f417a6924deee8b98bc47a
Max Blatt <max.blatt@web.de>
2016-05-04 21:20:26 +0200

    Remove field SERVER_ERROR in GWTEntryPoint because it is not used

--- JobTracker/src/net/greenbeansit/jobtracker/client/GwtEntryPoint.java
+++ JobTracker/src/net/greenbeansit/jobtracker/client/GwtEntryPoint.java
@@ -10,13 +10,6 @@ import com.google.gwt.user.client.ui.RootPanel;
 public class GwtEntryPoint implements EntryPoint
 {
 	/**
-	 * The message displayed to the user when the server cannot be reached or
-	 * returns an error.
-	 */
-	private static final String SERVER_ERROR = "An error occurred while "
-			+ "attempting to contact the server. Please check your network " + "connection and try again.";
-
-	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad()
