c77af4765317f843a36304b104705ec71c0a7e6b
Max Blatt <max.blatt@web.de>
2016-05-11 22:28:34 +0200

    Add classes ManagerPage and UserListItem
    Additionally, implement the link to the ManagerPage in the navbar.

---
+++ JobTracker/src/net/greenbeansit/jobtracker/client/components/ManagerPage.java
@@ -0,0 +1,58 @@
+package net.greenbeansit.jobtracker.client.components;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.gwtbootstrap3.client.ui.Anchor;
+import org.gwtbootstrap3.client.ui.Row;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+
+import net.greenbeansit.jobtracker.client.components.widgets.UserListItem;
+import net.greenbeansit.jobtracker.shared.User;
+
+public class ManagerPage extends Composite
+{
+
+	private static ManagerPageUiBinder uiBinder = GWT
+			.create(ManagerPageUiBinder.class);
+
+	interface ManagerPageUiBinder extends UiBinder<Widget, ManagerPage>
+	{
+	}
+
+	@UiField
+	Row employeeList;
+	
+	
+	List<User> userlist;
+	
+	public ManagerPage()
+	{
+		initWidget(uiBinder.createAndBindUi(this));
+		
+		userlist = new ArrayList<User>();
+		
+		userlist.add(new User(0, "Walter", "von der Vogelweide"));
+		userlist.add(new User(0, "Peter", "Tauber"));
+		userlist.add(new User(0, "Kurt", "Beck"));
+		userlist.add(new User(0, "Rudolf", "Scharping"));
+		userlist.add(new User(0, "Peter", "Altmeier"));
+		userlist.add(new User(0, "Peter", "Tauber"));
+		
+		//Fill the display list
+		for(User user : userlist)
+		{
+			Anchor anchor = new Anchor();
+			anchor.add(new UserListItem(user));
+			anchor.setHref("#manager_detail/" + user.getId());
+			
+			employeeList.add(anchor);
+		}
+	}
+
+}
---
+++ JobTracker/src/net/greenbeansit/jobtracker/client/components/widgets/UserListItem.java
@@ -0,0 +1,47 @@
+package net.greenbeansit.jobtracker.client.components.widgets;
+
+import org.gwtbootstrap3.client.ui.ProgressBar;
+import org.gwtbootstrap3.client.ui.html.ClearFix;
+import org.gwtbootstrap3.client.ui.html.Span;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Anchor;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+
+import net.greenbeansit.jobtracker.shared.User;
+
+public class UserListItem extends Composite
+{
+
+	private static UserListItemUiBinder uiBinder = GWT
+			.create(UserListItemUiBinder.class);
+
+	interface UserListItemUiBinder extends UiBinder<Widget, UserListItem>
+	{
+	}
+
+	@UiField
+	ClearFix	anchor;
+
+	@UiField
+	Span		spanName;
+
+	@UiField
+	Span		spanUtilization;
+
+	@UiField
+	ProgressBar	barUtilization;
+
+	public UserListItem(User user)
+	{
+		initWidget(uiBinder.createAndBindUi(this));
+
+		spanName.setText(user.getName());
+		spanUtilization.setText("65 %");
+		barUtilization.setPercent(65);
+	}
+
+}
