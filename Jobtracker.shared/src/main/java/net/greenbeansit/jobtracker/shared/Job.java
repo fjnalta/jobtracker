1ddcba7f057e09a0afe704192432b0a19eb26c18
Philipp Minges <fjnalta@gmail.com>
2016-05-13 15:21:43 +0200

    add setter for json parsing

--- Jobtracker.shared/src/main/java/net/greenbeansit/jobtracker/shared/Job.java
+++ Jobtracker.shared/src/main/java/net/greenbeansit/jobtracker/shared/Job.java
@@ -5,18 +5,18 @@ import java.io.Serializable;
 /**
  * Represents a job of a project. It is used in frontend.
  * 
- * @author Mike Hukiewitz & Alex & Philipp
+ * @author Mike Hukiewitz & Alex & Philipp Minges
  */
 public class Job implements Serializable {
 	private static final long serialVersionUID = -3379608733084915877L;
 
-	private Integer maxBudget;
-	private Integer usedBudget;
-	private Integer payMode;
 	private Integer jobNr, posNr;
+	private Integer payMode;
 	private Integer customerID;
 	private String desc;
-
+	private Integer maxBudget;
+	private Integer usedBudget;
+	
 	/**
 	 * Initializes a new instance of the {@link Job} class with all its fields
 	 * set to null.
@@ -59,6 +59,48 @@ public class Job implements Serializable {
 	 */
 	public Integer getMaxBudget() {
 		return maxBudget;
+	}
+
+	public void setJobNr(Integer jobNr) {
+		if(jobNr == null)
+			throw new IllegalArgumentException();
+		this.jobNr = jobNr;
+	}
+
+	public void setPosNr(Integer posNr) {
+		if(posNr == null)
+			throw new IllegalArgumentException();
+		this.posNr = posNr;
+	}
+
+	public void setPayMode(Integer payMode) {
+		if(payMode == null)
+			throw new IllegalArgumentException();
+		this.payMode = payMode;
+	}
+
+	public void setCustomerID(Integer customerID) {
+		if(customerID == null)
+			throw new IllegalArgumentException();
+		this.customerID = customerID;
+	}
+
+	public void setDesc(String desc) {
+		if(desc == "")
+			throw new IllegalArgumentException();
+		this.desc = desc;
+	}
+
+	public void setMaxBudget(Integer maxBudget) {
+		if(maxBudget == null)
+			throw new IllegalArgumentException();
+		this.maxBudget = maxBudget;
+	}
+
+	public void setUsedBudget(Integer usedBudget) {
+		if(usedBudget == null)
+			throw new IllegalArgumentException();
+		this.usedBudget = usedBudget;
 	}
 
 	public Integer getUsedBudget() {
