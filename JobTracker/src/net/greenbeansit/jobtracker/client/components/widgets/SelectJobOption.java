5659bdf71f1f9bad49657cd716dc10c7d3ecdb26
Philipp Minges <fjnalta@gmail.com>
2016-05-18 17:35:19 +0200

    format job string

--- JobTracker/src/net/greenbeansit/jobtracker/client/components/widgets/SelectJobOption.java
+++ JobTracker/src/net/greenbeansit/jobtracker/client/components/widgets/SelectJobOption.java
@@ -7,10 +7,11 @@ import net.greenbeansit.jobtracker.shared.Job;
 public class SelectJobOption extends Option{
 	
 	private Job job;
+
 	
 	public SelectJobOption(Job job){
 		this.setJob(job);
-		this.setText(String.valueOf(job.getJobNr()));
+		this.setText("JobNr: " + String.valueOf(job.getJobNr()) + " | Descr: " + job.getDesc());
 	}
 
 	public Job getJob() {
