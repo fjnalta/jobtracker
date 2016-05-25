e3845ad5749169fce56a38a7d005ba9e4aa2915b
Philipp Minges <fjnalta@gmail.com>
2016-05-25 17:22:18 +0200

    comment changed services to prevent compilation errors.

--- JobTracker.backend/src/main/java/net/greenbeansit/jobtracker/server/data/utilizationWeek/UtilizationWeekServiceJpa.java
+++ JobTracker.backend/src/main/java/net/greenbeansit/jobtracker/server/data/utilizationWeek/UtilizationWeekServiceJpa.java
@@ -10,87 +10,79 @@ import org.springframework.stereotype.Service;
 import net.greenbeansit.jobtracker.shared.UtilizationWeek;
 
 @Service("utilizationWeekService")
-public class UtilizationWeekServiceJpa implements UtilizationWeekDataService
-{
+public class UtilizationWeekServiceJpa implements UtilizationWeekDataService {
 
-	@Autowired
-	private UtilizationWeekEntityRepository repository;
+    @Autowired
+    private UtilizationWeekEntityRepository repository;
 
-	@Override
-	public List<UtilizationWeek> getAll()
-	{
-		ArrayList<UtilizationWeek> list = new ArrayList<UtilizationWeek>();
-		for (UtilizationWeekEntity entity : repository.findAll())
-		{
-			list.add(convert(entity));
-		}
-		return list;
-	}
+    @Override
+    public List<UtilizationWeek> getAll() {
+        ArrayList<UtilizationWeek> list = new ArrayList<UtilizationWeek>();
+        for (UtilizationWeekEntity entity : repository.findAll()) {
+            list.add(convert(entity));
+        }
+        return list;
+    }
 
-	@Override
-	public UtilizationWeek getUtilizationWeek(Integer utilId)
-	{
-		return convert(repository.findById(utilId));
-	}
+    @Override
+    public UtilizationWeek getUtilizationWeek(Integer utilId) {
+        return convert(repository.findById(utilId));
+    }
 
-	@Override
-	public List<UtilizationWeek> getByPeriod(Date from, Date to)
-	{
-		//TODO Mike: Implement when repository done
-		return null;
-	}
-	
-	@Override
-	public List<UtilizationWeek> getByPseudoJob(Integer pseudoJobId)
-	{
-		ArrayList<UtilizationWeek> list = new ArrayList<UtilizationWeek>();
-		for (UtilizationWeekEntity entity : repository.findByPseudoJobId(pseudoJobId))
-		{
-			list.add(convert(entity));
-		}
-		return list;
-	}
+    @Override
+    public List<UtilizationWeek> getByPeriod(Date from, Date to) {
+        //TODO Mike: Implement when repository done
+        return null;
+    }
 
-	@Override
-	public List<UtilizationWeek> getByUser(Integer userId)
-	{
-		ArrayList<UtilizationWeek> list = new ArrayList<UtilizationWeek>();
-		for (UtilizationWeekEntity entity : repository.findByAuthor(userId))
-		{
-			list.add(convert(entity));
-		}
-		return list;
-	}
+    @Override
+    public List<UtilizationWeek> getByPseudoJob(Integer pseudoJobId) {
+        ArrayList<UtilizationWeek> list = new ArrayList<UtilizationWeek>();
+        for (UtilizationWeekEntity entity : repository.findByPseudoJobId(pseudoJobId)) {
+            list.add(convert(entity));
+        }
+        return list;
+    }
 
-	@Override
-	public boolean save(UtilizationWeek util)
-	{
-		return repository.save(convert(util)) != null;
-	}
+    @Override
+    public List<UtilizationWeek> getByUser(Integer userId) {
+        ArrayList<UtilizationWeek> list = new ArrayList<UtilizationWeek>();
+        for (UtilizationWeekEntity entity : repository.findByAuthor(userId)) {
+            list.add(convert(entity));
+        }
+        return list;
+    }
 
-	@Override
-	public void delete(UtilizationWeek util)
-	{
-		repository.delete(convert(util));
-	}
+    @Override
+    public boolean save(UtilizationWeek util) {
+        return repository.save(convert(util)) != null;
+    }
 
-	private UtilizationWeek convert(UtilizationWeekEntity entity)
-	{
-		if (entity == null)
-			return null;
-		return new UtilizationWeek(entity.getId(), entity.getAuthor(),
-				entity.getBegin(), entity.getDaysFree(), entity.getDaysWork(),
-				entity.getDaysHoliday(), entity.getPossibilty(),
-				entity.getPseudoJobId());
-	}
+    @Override
+    public void delete(UtilizationWeek util) {
+        repository.delete(convert(util));
+    }
 
-	private UtilizationWeekEntity convert(UtilizationWeek util)
-	{
-		if (util == null)
-			return null;
-		return new UtilizationWeekEntity(util.getId(), util.getAuthor(),
-				util.getBegin(), util.getDaysFree(), util.getDaysWork(),
-				util.getDaysHoliday(), util.getPossibilty(),
-				util.getPseudoJobId());
-	}
+
+    //TODO change UtilizationWeekEntity Constructor in Backend
+
+    private UtilizationWeek convert(UtilizationWeekEntity entity) {
+        if (entity == null)
+            return null;
+//		return new UtilizationWeek(entity.getId(), entity.getAuthor(),
+//				entity.getBegin(), entity.getDaysFree(), entity.getDaysWork(),
+//				entity.getDaysHoliday(), entity.getPossibilty(),
+//				entity.getPseudoJobId());
+        return null;
+    }
+
+    private UtilizationWeekEntity convert(UtilizationWeek util) {
+        if (util == null)
+            return null;
+//		return new UtilizationWeekEntity(util.getId(), util.getAuthor(),
+//				util.getBegin(), util.getDaysFree(), util.getDaysWork(),
+//				util.getDaysHoliday(), util.getPossibilty(),
+//				util.getPseudoJobId());
+        return null;
+    }
 }
--- JobTracker.backend/src/main/java/net/greenbeansit/jobtracker/server/rest/services/RestServiceImpl.java
+++ JobTracker.backend/src/main/java/net/greenbeansit/jobtracker/server/rest/services/RestServiceImpl.java
@@ -21,184 +21,181 @@ import net.greenbeansit.jobtracker.shared.rest.services.RestService.ManagerPageR
 
 /**
  * Dummy implementation of the {@link RestService} interface.
- * 
+ *
  * @author Max Blatt & Alexander Kirilyuk & Philipp Minges
  */
-public class RestServiceImpl implements RestService
-{
-	@Inject
-	private ActivityReportDataService activityReportService;
-	@Inject
-	private ActivityReportTemplateDataService activityReportTemplateService;
-	@Inject
-	private CustomerDataService customerService;
-	@Inject
-	private JobDataService jobService;
-	@Inject
-	private UserDataService userService;
-	@Inject
-	private UserJobDataService userJobService;
+public class RestServiceImpl implements RestService {
+    @Inject
+    private ActivityReportDataService activityReportService;
+    @Inject
+    private ActivityReportTemplateDataService activityReportTemplateService;
+    @Inject
+    private CustomerDataService customerService;
+    @Inject
+    private JobDataService jobService;
+    @Inject
+    private UserDataService userService;
+    @Inject
+    private UserJobDataService userJobService;
 
-	/**
-	 * Empty Constructor for Spring mapping
-	*/
-	public RestServiceImpl()
-	{
-		
-	}
+    /**
+     * Empty Constructor for Spring mapping
+     */
+    public RestServiceImpl() {
 
-	//User
-	@Override
-	public List<User> getAllUser() {
-		return userService.getAll();
-	}
-	
-	@Override
-	public User getUser(Integer id)
-	{
-		return userService.getUser(id);
-	}
-	
-	//Jobs
-	@Override
-	public List<Job> getAllJobs(Integer userId)
-	{
-		//TODO: Show only those that the user may access
-		return jobService.getByUser(userId);
-	}
+    }
 
-	//Activity Reports
-	@Override
-	public List<ActivityReport> getAllReports(Integer userId)
-	{
-		return activityReportService.getByUser(userId);
-	}
+    //User
+    @Override
+    public List<User> getAllUser() {
+        return userService.getAll();
+    }
 
-	@Override
-	public ActivityReport getReport(Integer userId, Integer reportId)
-	{
-		ActivityReport report = activityReportService.getActivityReport(reportId);
-		if(report.getAuthor().equals(userId))
-			return report;
-		else
-			return null; //TODO: Throw error if not enough permission
-	}
+    @Override
+    public User getUser(Integer id) {
+        return userService.getUser(id);
+    }
+
+    //Jobs
+    @Override
+    public List<Job> getAllJobs(Integer userId) {
+        //TODO: Show only those that the user may access
+        return jobService.getByUser(userId);
+    }
+
+    //Activity Reports
+    @Override
+    public List<ActivityReport> getAllReports(Integer userId) {
+        return activityReportService.getByUser(userId);
+    }
+
+    @Override
+    public ActivityReport getReport(Integer userId, Integer reportId) {
+        ActivityReport report = activityReportService.getActivityReport(reportId);
+        if (report.getAuthor().equals(userId))
+            return report;
+        else
+            return null; //TODO: Throw error if not enough permission
+    }
 
 
-	@Override
-	public void saveReport(Integer userId, ActivityReport report)
-	{
+    @Override
+    public void saveReport(Integer userId, ActivityReport report) {
 //		report.setAuthor(userId);
-		activityReportService.save(report);
-		userService.updateYearUtilization(userId, report.getDate().getYear());
-		System.out.println(report);
-	}
+        activityReportService.save(report);
+        userService.updateYearUtilization(userId, report.getDate().getYear());
+        System.out.println(report);
+    }
 
-	@Override
-	public void deleteReport(Integer userId, Integer reportId)
-	{
-		ActivityReport report = activityReportService.getActivityReport(reportId);
-		if(report.getAuthor().equals(userId))
-			activityReportService.delete(report);
-		//TODO: Throw error if not enough permission
-	}
+    @Override
+    public void deleteReport(Integer userId, Integer reportId) {
+        ActivityReport report = activityReportService.getActivityReport(reportId);
+        if (report.getAuthor().equals(userId))
+            activityReportService.delete(report);
+        //TODO: Throw error if not enough permission
+    }
 
-	//Activity Report Templates
-	@Override
-	public List<ActivityReportTemplate> getAllReportTemplates(Integer userId)
-	{
-		return activityReportTemplateService.getBy(userId);
-	}
+    //Activity Report Templates
+    @Override
+    public List<ActivityReportTemplate> getAllReportTemplates(Integer userId) {
+        return activityReportTemplateService.getBy(userId);
+    }
 
-	@Override
-	public void saveReportTemplate(Integer userId,
-			ActivityReportTemplate template)
-	{
-		template.setAuthor(userId);
-		activityReportTemplateService.save(template);
-	}
+    @Override
+    public void saveReportTemplate(Integer userId,
+                                   ActivityReportTemplate template) {
+        template.setAuthor(userId);
+        activityReportTemplateService.save(template);
+    }
 
-	@Override
-	public void deleteReportTemplate(Integer author, String name)
-	{
-		ActivityReportTemplate template = activityReportTemplateService.getTemplate(author, name);
-		if(template.getAuthor().equals(author) && template.getName().equals(name))
-			activityReportTemplateService.delete(template);
-	}
+    @Override
+    public void deleteReportTemplate(Integer author, String name) {
+        ActivityReportTemplate template = activityReportTemplateService.getTemplate(author, name);
+        if (template.getAuthor().equals(author) && template.getName().equals(name))
+            activityReportTemplateService.delete(template);
+    }
 
-	//Customer
-	@Override
-	public List<Customer> getAllCustomer() {
-		return customerService.getAll();
-	}
+    //Customer
+    @Override
+    public List<Customer> getAllCustomer() {
+        return customerService.getAll();
+    }
 
-	@Override
-	public Customer getCustomer(Integer id) {
-		return customerService.getById(id);
-	}
+    @Override
+    public Customer getCustomer(Integer id) {
+        return customerService.getById(id);
+    }
 
 
-	@SuppressWarnings("deprecation")
-	private Date stringToDate(String date) {
-		return new Date(Integer.parseInt(date.substring(0, 4))-1900,Integer.parseInt(date.substring(5, 7))-1, Integer.parseInt(date.substring(8)));
-	}
+    @SuppressWarnings("deprecation")
+    private Date stringToDate(String date) {
+        return new Date(Integer.parseInt(date.substring(0, 4)) - 1900, Integer.parseInt(date.substring(5, 7)) - 1, Integer.parseInt(date.substring(8)));
+    }
 
-	@Override
-	public List<UserJob> getUsersToJob(Integer jobNo, Integer posNo) {
-		return userJobService.getByJobNrAndPosNr(jobNo, posNo);
-	}
+    @Override
+    public List<UserJob> getUsersToJob(Integer jobNo, Integer posNo) {
+        return userJobService.getByJobNrAndPosNr(jobNo, posNo);
+    }
 
-	@Override
-	public List<ActivityReport> getReportPeriod(Integer userId, String fromto)
-	{
-		return activityReportService.getByUserAndPeriod(userId, stringToDate(fromto.substring(0, 10)), stringToDate(fromto.substring(11)));
-	}
+    @Override
+    public List<ActivityReport> getReportPeriod(Integer userId, String fromto) {
+        return activityReportService.getByUserAndPeriod(userId, stringToDate(fromto.substring(0, 10)), stringToDate(fromto.substring(11)));
+    }
 
-	@Override
-	public Customer getCustomer(String name)
-	{
-		return customerService.getByName(name);
-	}
+    @Override
+    public Customer getCustomer(String name) {
+        return customerService.getByName(name);
+    }
 
-	@Override
-	public ManagerPageRestServiceResponse getEmployees(Integer supervisorId)
-	{
-		List<User> users = userService.getBySupervisor(supervisorId);
-		List<Job> jobs = new ArrayList<Job>();
-		for(User user : users)
-		{
-			List<Job> temp = jobService.getByUser(user.getId());
-			if (temp != null)
-			{
-				List<JobID> keys = new ArrayList<JobID>();
-				for (Job job : temp)
-				{
-					keys.add(new JobID(job.getJobNr(), job.getPosNr()));
-					if (!jobs.contains(job))
-						jobs.add(job);
-				}
-				user.setAssignedJobs(keys);
-			}
-		}
-		return new ManagerPageRestServiceResponse(users, jobs);
-	}
+    @Override
+    public ManagerPageRestServiceResponse getEmployees(Integer supervisorId) {
+        List<User> users = userService.getBySupervisor(supervisorId);
+        List<Job> jobs = new ArrayList<Job>();
+        for (User user : users) {
+            List<Job> temp = jobService.getByUser(user.getId());
+            if (temp != null) {
+                List<JobID> keys = new ArrayList<JobID>();
+                for (Job job : temp) {
+                    keys.add(new JobID(job.getJobNr(), job.getPosNr()));
+                    if (!jobs.contains(job))
+                        jobs.add(job);
+                }
+                user.setAssignedJobs(keys);
+            }
+        }
+        return new ManagerPageRestServiceResponse(users, jobs);
+    }
 
-	@Override
-	public Integer getUtilizationMonth(Integer userId, Integer year,
-			Integer month)
-	{
-		//Sooooo dirty...
-		Date from = new Date(year-1900, month-1, 1);
-		Date to = new Date(year-1900, month-1, 30);
-		return userService.getUtilization(userId, from, to);
-	}
+    //TODO
+    @Override
+    public List<PseudoJob> getAllPseudoJobs(Integer userId) {
+        return null;
+    }
 
-	@Override
-	public Integer getUtilizationYear(Integer userId, Integer year)
-	{
-		//Oh gaaaaawd...
-		Date from = new Date(year-1900, 0, 1);
-		Date to = new Date(year-1900, 11, 30);
-		return userService.getUtilization(userId, from, to);
-	}
+    @Override
+    public void savePseudoJob(Integer userId, PseudoJob pseudoJob) {
+
+    }
+
+    @Override
+    public void deletePseudoJob(Integer userId, Integer pseudoJobId) {
+
+    }
+
+    @Override
+    public Integer getUtilizationMonth(Integer userId, Integer year,
+                                       Integer month) {
+        //Sooooo dirty...
+        Date from = new Date(year - 1900, month - 1, 1);
+        Date to = new Date(year - 1900, month - 1, 30);
+        return userService.getUtilization(userId, from, to);
+    }
+
+    @Override
+    public Integer getUtilizationYear(Integer userId, Integer year) {
+        //Oh gaaaaawd...
+        Date from = new Date(year - 1900, 0, 1);
+        Date to = new Date(year - 1900, 11, 30);
+        return userService.getUtilization(userId, from, to);
+    }
 }
