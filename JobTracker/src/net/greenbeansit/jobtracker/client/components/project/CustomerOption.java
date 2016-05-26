88d286f8847f297baeb700ef59844f3a9468b1c6
Max Blatt <max.blatt@web.de>
2016-05-26 20:34:46 +0200

    Change layout of the ProjectPage to the same as the ManagerPage

---
+++ JobTracker/src/net/greenbeansit/jobtracker/client/components/project/CustomerOption.java
@@ -0,0 +1,22 @@
+package net.greenbeansit.jobtracker.client.components.project;
+
+import org.gwtbootstrap3.extras.select.client.ui.Option;
+
+import net.greenbeansit.jobtracker.shared.Customer;
+
+class CustomerOption extends Option
+{
+	private Customer customer;
+	
+	public CustomerOption(Customer customer)
+	{
+		this.setTitle(customer.getName());
+		this.customer = customer;
+	}
+	
+	public Customer getCustomer()
+	{
+		return customer;
+	}
+}
+
