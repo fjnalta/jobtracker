3d1ce187b1bee2268832aaaaa12b5acb91b42a22
Philipp Minges <fjnalta@gmail.com>
2016-05-27 14:14:10 +0200

    implement start and end date

--- JobTracker/src/net/greenbeansit/jobtracker/client/components/kapa/KapaPage.java
+++ JobTracker/src/net/greenbeansit/jobtracker/client/components/kapa/KapaPage.java
@@ -9,12 +9,16 @@ import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
+import net.greenbeansit.jobtracker.client.components.CalendarObserver;
 import net.greenbeansit.jobtracker.client.components.LogicObservable;
 import net.greenbeansit.jobtracker.client.components.widgets.SelectJobOption;
 import net.greenbeansit.jobtracker.client.utils.rest.NotifyHelper;
 import net.greenbeansit.jobtracker.shared.Job;
 import net.greenbeansit.jobtracker.shared.PseudoJob;
-import org.gwtbootstrap3.client.ui.*;
+import org.gwtbootstrap3.client.ui.Button;
+import org.gwtbootstrap3.client.ui.Heading;
+import org.gwtbootstrap3.client.ui.Row;
+import org.gwtbootstrap3.client.ui.TextBox;
 import org.gwtbootstrap3.client.ui.constants.IconType;
 import org.gwtbootstrap3.extras.fullcalendar.client.ui.FullCalendar;
 import org.gwtbootstrap3.extras.fullcalendar.client.ui.ViewOption;
@@ -30,7 +34,7 @@ import java.util.List;
 /**
  * Created by Philipp Minges on 23.05.16.
  */
-public class KapaPage extends Composite implements LogicObservable {
+public class KapaPage extends Composite implements CalendarObserver, LogicObservable {
 
     @UiField
     OptGroup myJobsOptGroup,allJobsOptGroup;
@@ -39,7 +43,11 @@ public class KapaPage extends Composite implements LogicObservable {
     Select selectJob;
 
     @UiField
-    Button buttonUp, buttonDown, buttonSave;
+    Button buttonUp, buttonDown, buttonUpYearStart, buttonDownYearStart,
+            buttonUpCalendarWeekStart, buttonDownCalendarWeekStart,
+            buttonUpYearEnd, buttonDownYearEnd, buttonUpDayWeek,
+            buttonDownDayWeek, buttonUpCalendarWeekEnd, buttonDownCalendarWeekEnd,
+            buttonSave;
 
     @UiField
     TextBox possibilityPercentage, textIdentifier;
@@ -95,7 +103,7 @@ public class KapaPage extends Composite implements LogicObservable {
 
     private static KapaPageUiBinder uiBinder = GWT.create(KapaPageUiBinder.class);
 
-    FullCalendar fc = new FullCalendar("kapaCalendar", ViewOption.month, false);
+    FullCalendar fc = new FullCalendar("kapaCalendar", ViewOption.month, true);
 
     public KapaPage() {
         initWidget(uiBinder.createAndBindUi(this));
@@ -115,9 +123,7 @@ public class KapaPage extends Composite implements LogicObservable {
     }
 
     private void initialize() {
-        //set Buttons
-        buttonDown.setIcon(IconType.ARROW_DOWN);
-        buttonUp.setIcon(IconType.ARROW_UP);
+        setButtons();
         //set Slider
         possibilityPercentage.setText(mySlider.getValue().toString());
         //load calendar
@@ -156,5 +162,35 @@ public class KapaPage extends Composite implements LogicObservable {
         else{
             NotifyHelper.errorMessage("Please select job");
         }
+    }
+
+    @Override
+    public void update() {
+        fc.render();
+    }
+
+    @Override
+    public void notifyHandler() {
+        calendarHandler.updateObserver(this);
+    }
+
+    private void setButtons(){
+        buttonDown.setIcon(IconType.ARROW_DOWN);
+        buttonUp.setIcon(IconType.ARROW_UP);
+
+        buttonUpYearStart.setIcon(IconType.ARROW_UP);
+        buttonDownYearStart.setIcon(IconType.ARROW_DOWN);
+
+        buttonUpCalendarWeekStart.setIcon(IconType.ARROW_UP);
+        buttonDownCalendarWeekStart.setIcon(IconType.ARROW_DOWN);
+
+        buttonUpYearEnd.setIcon(IconType.ARROW_UP);
+        buttonDownYearEnd.setIcon(IconType.ARROW_DOWN);
+
+        buttonUpCalendarWeekEnd.setIcon(IconType.ARROW_UP);
+        buttonDownCalendarWeekEnd.setIcon(IconType.ARROW_DOWN);
+
+        buttonUpDayWeek.setIcon(IconType.ARROW_UP);
+        buttonDownDayWeek.setIcon(IconType.ARROW_DOWN);
     }
 }
