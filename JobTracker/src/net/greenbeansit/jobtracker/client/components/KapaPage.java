424faf2cc2228f086afd5f5539e004deeaefa3e0
Philipp Minges <fjnalta@gmail.com>
2016-05-27 11:23:29 +0200

    update KapaPage

--- JobTracker/src/net/greenbeansit/jobtracker/client/components/KapaPage.java
+++ JobTracker/src/net/greenbeansit/jobtracker/client/components/KapaPage.java
@@ -12,6 +12,7 @@ import com.google.gwt.user.client.ui.Widget;
 import net.greenbeansit.jobtracker.client.components.widgets.SelectJobOption;
 import net.greenbeansit.jobtracker.client.utils.rest.NotifyHelper;
 import net.greenbeansit.jobtracker.shared.Job;
+import net.greenbeansit.jobtracker.shared.PseudoJob;
 import org.gwtbootstrap3.client.ui.*;
 import org.gwtbootstrap3.client.ui.constants.IconType;
 import org.gwtbootstrap3.extras.fullcalendar.client.ui.FullCalendar;
@@ -30,9 +31,6 @@ import java.util.List;
  */
 public class KapaPage extends Composite implements LogicObservable {
 
-    private List<Job> jobList = new ArrayList<Job>();
-    private Job currentJob = null;
-
     @UiField
     OptGroup myJobsOptGroup;
 
@@ -42,45 +40,14 @@ public class KapaPage extends Composite implements LogicObservable {
     @UiField
     Select selectJob;
 
-    @Override
-    public void updateObservable() {
-        allJobsOptGroup.clear();
-        this.jobList = handler.getJobList();
-        addJobs(this.jobList);
-        currentJob = handler.getCurrentJob();
-        if (currentJob != null) {
-            for (Option opt : selectJob.getItems()) {
-                ((SelectJobOption) opt).setSelected(false);
-                if (((SelectJobOption) opt).getJob().equals(currentJob)) {
-                    opt.setSelected(true);
-                }
-            }
-        }
-        selectJob.refresh();
-    }
-
-    @Override
-    public void notifyLogicHandler() {
-        if(currentJob != null){
-            handler.setCurrentJob(currentJob);
-        }
-        else{
-            NotifyHelper.errorMessage("Please select job");
-        }
-    }
-
-    interface KapaPageUiBinder extends UiBinder<Widget, KapaPage> {
-    }
-
-    private static KapaPageUiBinder uiBinder = GWT.create(KapaPageUiBinder.class);
-
-    FullCalendar fc = new FullCalendar("kapaCalendar", ViewOption.month, false);
-
     @UiField
     Button buttonUp;
 
     @UiField
     Button buttonDown;
+
+    @UiField
+    Button buttonSave;
 
     @UiField
     TextBox possibilityPercentage;
@@ -89,15 +56,30 @@ public class KapaPage extends Composite implements LogicObservable {
     Slider mySlider;
 
     @UiField
+    TextBox textIdentifier;
+
+    @UiField
     Row content;
 
     @UiField
     Heading dateHeading;
 
     @UiHandler("mySlider")
-    void onSlide(SlideEvent<Double> event) {
+    void onSlide(SlideEvent<Double> event)
+    {
         possibilityPercentage.setText(event.getValue().toString());
+    }
 
+    @UiHandler("buttonSave")
+    public void savePseudoJob(final ClickEvent e){
+        PseudoJob template = new PseudoJob();
+        if(textIdentifier.getText().length()>0){
+            template.setName(textIdentifier.getText());
+            //TODO implement LogicHandler savePseudoJob
+            //handler.savePseudoJob(template);
+        }else{
+            NotifyHelper.errorMessage("Fill missing fields");
+        }
     }
 
     @UiHandler("buttonUp")
@@ -114,6 +96,17 @@ public class KapaPage extends Composite implements LogicObservable {
             mySlider.setValue(mySlider.getValue() - 25);
         possibilityPercentage.setText(mySlider.getValue().toString());
     }
+
+    private List<Job> jobList = new ArrayList<Job>();
+    private List<PseudoJob> pseudoJobList = new ArrayList<PseudoJob>();
+    private Job currentJob = null;
+
+    interface KapaPageUiBinder extends UiBinder<Widget, KapaPage> {
+    }
+
+    private static KapaPageUiBinder uiBinder = GWT.create(KapaPageUiBinder.class);
+
+    FullCalendar fc = new FullCalendar("kapaCalendar", ViewOption.month, false);
 
     public KapaPage() {
         initWidget(uiBinder.createAndBindUi(this));
@@ -146,6 +139,33 @@ public class KapaPage extends Composite implements LogicObservable {
         for (Job currentJob : jobList) {
             SelectJobOption tempOption = new SelectJobOption(currentJob);
             allJobsOptGroup.add(tempOption);
+        }
+    }
+
+    @Override
+    public void updateObservable() {
+        allJobsOptGroup.clear();
+        this.jobList = handler.getJobList();
+        addJobs(this.jobList);
+        currentJob = handler.getCurrentJob();
+        if (currentJob != null) {
+            for (Option opt : selectJob.getItems()) {
+                ((SelectJobOption) opt).setSelected(false);
+                if (((SelectJobOption) opt).getJob().equals(currentJob)) {
+                    opt.setSelected(true);
+                }
+            }
+        }
+        selectJob.refresh();
+    }
+
+    @Override
+    public void notifyLogicHandler() {
+        if(currentJob != null){
+            handler.setCurrentJob(currentJob);
+        }
+        else{
+            NotifyHelper.errorMessage("Please select job");
         }
     }
 }
