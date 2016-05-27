290c7fdf95a9319af6d904a2db007ead69ed2ee8
layton736 <ahmed.salame@stud.hs-mannheim.de>
2016-05-27 14:14:59 +0200

    Change CalendarTimeLineWidget

--- JobTracker/src/net/greenbeansit/jobtracker/client/components/widgets/calendar/CalendarTimeLineWidget.java
+++ JobTracker/src/net/greenbeansit/jobtracker/client/components/widgets/calendar/CalendarTimeLineWidget.java
@@ -15,6 +15,7 @@ import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.user.datepicker.client.CalendarUtil;
 
 import net.greenbeansit.jobtracker.client.components.CalendarObserver;
 
@@ -35,31 +36,58 @@ public class CalendarTimeLineWidget extends Composite implements CalendarObserve
 	private static CalendarTimeLineWidgetUiBinder uiBinder = GWT.create(CalendarTimeLineWidgetUiBinder.class);
 
 	interface CalendarTimeLineWidgetUiBinder extends UiBinder<Widget, CalendarTimeLineWidget> {
-
 	}
 
+	@SuppressWarnings("deprecation")
 	public CalendarTimeLineWidget() {
 		initWidget(uiBinder.createAndBindUi(this));
 		calendarHandler.addObserver(this);
 		String currentDate = createDate(new Date().toString());
 		setDate(currentDate);
-		setKalenderWeek("KW`16");
+		Date d = new Date();
+		setCalenderWeek(getCalendarWeek(d.getYear()+1900,d.getMonth() + 1, d.getDate()));
+	}
+
+	/**
+	 * Get the calendarweek from the given Date
+	 * @param year
+	 * @param month
+	 * @param day
+	 * @return
+	 */
+	// TODO- Schaltjahr berechnung
+	private String getCalendarWeek(int year, int month, int day) {
+		
+		int summDay = 0;
+		while(month > 0 ){
+			summDay += this.getMonthDayNumber(month--);
+		}
+		summDay /= 7;
+		return summDay+" KW";
 	}
 
 	public CalendarTimeLineWidget(String firstName) {
 		initWidget(uiBinder.createAndBindUi(this));
 		calendarHandler.addObserver(this);
 	}
-
+	
 	@Override
 	public void update() {
 		String[] sum = calendarHandler.calendar.currentEvent.getISOStart().split("T");
 		String[] sum2 = sum[0].split("-");
 		String day = sum2[2];
 		String month = getMonthName(sum2[1]);
-		date.setText(month + "`" + day);
+		
+		int yearInt = Integer.parseInt(sum2[0]);
+		int monthInt = Integer.parseInt(sum2[1]);
+		int dayInt = Integer.parseInt(sum2[2]);
+		
+		setDate(month + "`" + day);
+		Window.alert(yearInt +" "+ monthInt+ " " + dayInt);
+		setCalenderWeek(getCalendarWeek(yearInt, monthInt, dayInt));
+		
 	}
-
+	
 	private String getMonthName(String string) {
 		switch(string){
 		case"01":
@@ -88,8 +116,38 @@ public class CalendarTimeLineWidget extends Composite implements CalendarObserve
 			return "Dezember";
 		default:
 			return null;
+		}	
+	}
+	
+	private int getMonthDayNumber(int month){
+		switch(month){
+		case 1:
+			return 31;
+		case 2:
+			return 28;
+		case 3:
+			return 31;
+		case 4:
+			return 30;
+		case 5:
+			return 31;
+		case 6:
+			return 30;
+		case 7:
+			return 31;
+		case 8:
+			return 31;
+		case 9:
+			return 30;
+		case 10:
+			return 31;
+		case 11:
+			return 30;
+		case 12:
+			return 31;
+		default:
+			return -100;
 		}
-		
 	}
 
 	@Override
@@ -97,7 +155,7 @@ public class CalendarTimeLineWidget extends Composite implements CalendarObserve
 		calendarHandler.updateObserver(this);
 	}
 
-	public void setKalenderWeek(String week) {
+	public void setCalenderWeek(String week) {
 		kw.setText(week);
 	}
 
