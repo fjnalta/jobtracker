56b34af3fe22f28486582b7eb070a42432b0917e
Max Blatt <max.blatt@web.de>
2016-06-01 14:33:52 +0200

    Add JavaDoc to ManagerPageSortMode

--- JobTracker/src/net/greenbeansit/jobtracker/client/components/manager/ManagerPageSortMode.java
+++ JobTracker/src/net/greenbeansit/jobtracker/client/components/manager/ManagerPageSortMode.java
@@ -1,6 +1,29 @@
 package net.greenbeansit.jobtracker.client.components.manager;
 
+/**
+ * Defines sort modes for the {@link ManagerPage}.
+ * 
+ * @author Max Blatt
+ */
 enum ManagerPageSortMode
 {
-	ALPHABETICAL_UP, ALPHABETICAL_DOWN, UTILIZATION_UP, UTILIZATION_DOWN
+	/**
+	 * Sorts by the employee's name in upward direction.
+	 */
+	ALPHABETICAL_UP,
+
+	/**
+	 * Sorts by the employee's name in downward direction.
+	 */
+	ALPHABETICAL_DOWN,
+
+	/**
+	 * Sorts by the utilization of the employee in upward direction.
+	 */
+	UTILIZATION_UP,
+
+	/**
+	 * Sorts by the utilization of the employee in downward direction.
+	 */
+	UTILIZATION_DOWN
 }
