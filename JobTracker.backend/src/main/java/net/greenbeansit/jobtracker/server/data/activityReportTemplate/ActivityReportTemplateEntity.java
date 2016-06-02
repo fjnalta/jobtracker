399976a4b7b691378c9d8ef22ddb05c4c20884a3
Philipp Minges <fjnalta@gmail.com>
2016-06-02 12:09:25 +0200

    add JavaDoc of ActivityReportTemplateEntity

--- JobTracker.backend/src/main/java/net/greenbeansit/jobtracker/server/data/activityReportTemplate/ActivityReportTemplateEntity.java
+++ JobTracker.backend/src/main/java/net/greenbeansit/jobtracker/server/data/activityReportTemplate/ActivityReportTemplateEntity.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
  * used in backend. Each entity class requires a standard constructor and
  * getters/setters for usage in Java Spring.
  * 
- * @author Mike Hukiewitz
+ * @author Mike Hukiewitz & Philipp Minges
  *
  */
 @Entity
@@ -63,45 +63,77 @@ public class ActivityReportTemplateEntity implements Serializable
 		this.author = author;
 	}
 
+	/**
+	 * Gets the Name of the {@link ActivityReportTemplateEntity}
+	 * @return the name.
+     */
 	@Column(name = "name")
 	public String getName()
 	{
 		return name;
 	}
 
+	/**
+	 * Sets the Name of the {@link ActivityReportTemplateEntity}
+	 * @param name the Name of the {@link ActivityReportTemplateEntity}
+     */
 	public void setName(String name)
 	{
 		this.name = name;
 	}
 
+	/**
+	 * Gets the Text of the {@link ActivityReportTemplateEntity}
+	 * @return the Text of the {@link ActivityReportTemplateEntity}
+     */
 	@Column(name = "text")
 	public String getText()
 	{
 		return text;
 	}
 
+	/**
+	 * Sets the Text of the {@link ActivityReportTemplateEntity}
+	 * @param text the Text to be inserted into {@link ActivityReportTemplateEntity}
+     */
 	public void setText(String text)
 	{
 		this.text = text;
 	}
 
+	/**
+	 * Gets the Task id of the {@link ActivityReportTemplateEntity}
+	 * @return the Task id of the {@link ActivityReportTemplateEntity}
+     */
 	@Column(name = "task_id")
 	public Integer getTaskId()
 	{
 		return taskId;
 	}
 
+	/**
+	 * Sets the Task id of the {@link ActivityReportTemplateEntity}
+	 * @param taskId the Task id of the {@link ActivityReportTemplateEntity}
+     */
 	public void setTaskId(Integer taskId)
 	{
 		this.taskId = taskId;
 	}
 
+	/**
+	 * Gets the Author of the {@link ActivityReportTemplateEntity}
+	 * @return the Author of the {@link ActivityReportTemplateEntity}
+     */
 	@Column(name = "author")
 	public Integer getAuthor()
 	{
 		return author;
 	}
 
+	/**
+	 * Sets the Author of the {@link ActivityReportTemplateEntity}
+	 * @param author the Author of the {@link ActivityReportTemplateEntity}
+     */
 	public void setAuthor(Integer author)
 	{
 		this.author = author;
