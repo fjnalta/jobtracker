5892735da550dbc09c2211400f0a77f937410e97
Alexander Kirilyuk <alexander.kiri@hotmail.de>
2016-06-02 10:42:16 +0200

    update javadoc ActivityReportEvent update javadoc ActivityReportTemplate

--- Jobtracker.shared/src/main/java/net/greenbeansit/jobtracker/shared/ActivityReportTemplate.java
+++ Jobtracker.shared/src/main/java/net/greenbeansit/jobtracker/shared/ActivityReportTemplate.java
@@ -30,6 +30,13 @@ public class ActivityReportTemplate implements Serializable
 
 	}
 
+	/**
+	 * building constructor of {@link ActivityReportTemplate}
+	 * @param name String name of the template
+	 * @param text String text of the template
+	 * @param taskId Integer task id for the template
+     * @param author Integer authorID of this template
+     */
 	public ActivityReportTemplate(String name, String text, Integer taskId, Integer author) {
 		this.name = name;
 		this.text = text;
@@ -37,11 +44,19 @@ public class ActivityReportTemplate implements Serializable
 		this.author = author;
 	}
 
+	/**
+	 * get the name of this template
+	 * @return String value of name
+     */
 	public String getName()
 	{
 		return name;
 	}
 
+	/**
+	 * set the namme of this template
+	 * @param name String value of name
+     */
 	public void setName(String name)
 	{
 		if (name == "")
@@ -49,36 +64,65 @@ public class ActivityReportTemplate implements Serializable
 		this.name = name;
 	}
 
+	/**
+	 * get the Text
+	 * @return String value
+     */
 	public String getText()
 	{
 		return text;
 	}
 
+	/**
+	 * set the text
+	 * @param text String value
+     */
 	public void setText(String text)
 	{
 		this.text = text;
 	}
 
+	/**
+	 * get the taskID
+	 * @return Integer value
+     */
 	public Integer getTaskId()
 	{
 		return taskId;
 	}
 
+	/**
+	 * set the taskID
+	 * @param taskId Integer value
+     */
 	public void setTaskId(Integer taskId)
 	{
 		this.taskId = taskId;
 	}
 
+	/**
+	 * get the authorID
+	 * @return String value
+     */
 	public Integer getAuthor()
 	{
 		return author;
 	}
 
+	/**
+	 * set the authorID
+	 * @param author Integer value
+     */
 	public void setAuthor(Integer author)
 	{
 		this.author = author;
 	}
-	
+
+	/**
+	 * custom equals method
+	 * @param obj Object to compare to
+	 * @return Boolean value
+     */
 	public boolean equals(Object obj) {
 		if(obj == null)
 			return false;
