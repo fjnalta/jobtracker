ba1f6b46a07e87626fb84d94080bc0d926b2ab87
Alexander Kirilyuk <alexander.kiri@hotmail.de>
2016-06-02 12:38:40 +0200

    update ActivityReportEvent javadoc update ActivityReportServiceJpa javadoc update CalendarHandler javadoc

--- JobTracker/src/net/greenbeansit/jobtracker/client/components/CalendarHandler.java
+++ JobTracker/src/net/greenbeansit/jobtracker/client/components/CalendarHandler.java
@@ -19,7 +19,9 @@ import net.greenbeansit.jobtracker.client.components.widgets.calendar.FullCalend
  *
  */
 public class CalendarHandler implements LogicObservable {
-
+	/**
+	 * calendar
+	 */
 	public FullCalendarCustomize calendar;
 	//public Event currentEvent;
 
@@ -27,7 +29,7 @@ public class CalendarHandler implements LogicObservable {
 
 	/**
 	 * 
-	 * @param w
+	 * @param w add a calendar observer
 	 */
 	public void addObserver(CalendarObserver w) {
 
@@ -40,7 +42,7 @@ public class CalendarHandler implements LogicObservable {
 	}
 
 	/**
-	 * 
+	 * update a observer
 	 */
 	public void updateObserver(CalendarObserver obs) {
 		for (CalendarObserver p : list) {
@@ -53,13 +55,17 @@ public class CalendarHandler implements LogicObservable {
 	}
 
 	/**
-	 * 
-	 * @param w
+	 * delete a calendar observer
+	 * @param w observer to delete
 	 */
 	public void deleteObserver(CalendarObserver w) {
 		list.remove(w);
 	}
 
+	/**
+	 * register a calender to this handler
+	 * @param calenda the calendar to add
+     */
 	public void registerCalendar(FullCalendarCustomize calenda) {
 		this.calendar = calenda;
 	}
@@ -69,6 +75,9 @@ public class CalendarHandler implements LogicObservable {
 		updateAllObservables();
 	}
 
+	/**
+	 * update all observables
+	 */
 	private void updateAllObservables() {
 		for (CalendarObserver p : list) {
 			p.update();
@@ -82,8 +91,8 @@ public class CalendarHandler implements LogicObservable {
 	
 	/**
 	 * 
-	 * @param date
-	 * @param time
+	 * @param date the date
+	 * @param time the time(hours and minutes) in minutes Integer value
 	 * @return yyyy-mm-ddThh:mm:ss.000Z
 	 */
 	public String getISO8601StringForDate(Date date, int time) {
@@ -94,8 +103,8 @@ public class CalendarHandler implements LogicObservable {
 
 	/**
 	 * Add leading zero if number < 10
-	 * @param num
-	 * @return
+	 * @param num the number
+	 * @return String with a leading 0
 	 */
 	private String fillLeadingZero(int num) {
 		if (num < 10) {
@@ -104,11 +113,20 @@ public class CalendarHandler implements LogicObservable {
 		return num + "";
 	}
 
-	
+	/**
+	 * get the minutes
+	 * @param time the time
+	 * @return the minutes
+     */
 	private int calculateMinutes(int time) {
 		return time % 60;
 	}
 
+	/**
+	 * get the hours
+	 * @param time the hours value in minutes
+	 * @return the hours value
+     */
 	private int calculateHours(int time) {
 		return time / 60;
 	}
