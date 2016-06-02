28cefd24c78776deda80ec9db9746e05b2d299a9
Philipp Minges <fjnalta@gmail.com>
2016-06-02 10:37:52 +0200

    add JavaDoc to CapacityReportEvent

--- JobTracker/src/net/greenbeansit/jobtracker/client/components/kapa/data/CapacityReportEvent.java
+++ JobTracker/src/net/greenbeansit/jobtracker/client/components/kapa/data/CapacityReportEvent.java
@@ -3,15 +3,36 @@ package net.greenbeansit.jobtracker.client.components.kapa.data;
 import com.google.gwt.core.client.JavaScriptObject;
 import net.greenbeansit.jobtracker.shared.UtilizationWeek;
 import org.gwtbootstrap3.extras.fullcalendar.client.ui.*;
+import net.greenbeansit.jobtracker.client.components.kapa.widgets.CapacityCalendarWidget;
 
 /**
- * Created by Philipp Minges on 01.06.16.
+ * A Special {@link Event} for the {@link CapacityCalendarWidget}. It is used to display
+ * the {@link UtilizationWeek} on the {@link CapacityCalendarWidget} It inherits from
+ * {@link Event}.
+ *
+ * @author Philipp Minges
+ *
  */
 public class CapacityReportEvent extends Event {
 
     private UtilizationWeek uw;
     private Integer possibility;
 
+    /**
+     *
+     * @param uw
+     *          an {@link UtilizationWeek}
+     * @param id
+     *          the id of the {@link UtilizationWeek}
+     * @param title
+     *          the title of the {@link UtilizationWeek}
+     * @param isEditable
+     *          Determines whether the event on the calendar can be modified.
+     * @param isStartEditable
+     *          Allow events' start times to be editable through dragging.
+     * @param isDurationEditable
+     *          Allow events' durations to be editable through resizing.
+     */
     public CapacityReportEvent(UtilizationWeek uw, String id, String title, boolean isEditable,
                                boolean isStartEditable, boolean isDurationEditable) {
 
@@ -19,26 +40,59 @@ public class CapacityReportEvent extends Event {
         this.uw = uw;
     }
 
+    /**
+     * Initializes a new Instance of {@link CapacityReportEvent} with the following values
+     * @param id
+     *          the id of the {@link CapacityReportEvent}
+     * @param title
+     *          the title of the {@link CapacityReportEvent}
+     */
     public CapacityReportEvent(String id, String title) {
         super(id, title);
     }
 
+    /**
+     * Initializes a new instance of the {@link CapacityReportEvent} class with
+     * the following values
+     *
+     * @param jso
+     *            as a {@link JavaScriptObject}
+     */
     public CapacityReportEvent(JavaScriptObject jso) {
         super(jso);
     }
 
+    /**
+     * Method for getting the {@link UtilizationWeek} of the
+     * {@link CapacityReportEvent}.
+     *
+     * @return the {@link UtilizationWeek} of the {@link CapacityReportEvent}
+     */
     public UtilizationWeek getUw() {
         return uw;
     }
 
+    /**
+     * Method for setting the {@link UtilizationWeek} of the
+     * @param uw
+     *          the {@link UtilizationWeek}
+     */
     public void setUtilizationWeek(UtilizationWeek uw) {
         this.uw = uw;
     }
 
+    /**
+     * Method for getting the possibility of the {@link CapacityReportEvent}
+     * @return
+     */
     public int getPossibility() {
         return this.possibility;
     }
 
+    /**
+     * Method for setting the possibility of the {@link CapacityReportEvent}
+     * @param possibility
+     */
     public void setPossibility(Integer possibility) {
         this.possibility = possibility;
     }
