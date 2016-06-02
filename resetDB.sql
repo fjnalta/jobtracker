b5d0b06cf6f73c064875f5f46f3a2195017b2700
Philipp Minges <fjnalta@gmail.com>
2016-06-02 11:30:07 +0200

    write sqldumpfile

--- resetDB.sql
+++ resetDB.sql
@@ -1,2 +1,4 @@
-DROP DATABASE jobtracker;
+DROP DATABASE IF EXISTS jobtracker;
 CREATE DATABASE jobtracker;
+GRANT ALL ON jobtracker.* to 'greenbeansit'@'localhost' IDENTIFIED BY 'databasepw';
+FLUSH PRIVILEGES;
